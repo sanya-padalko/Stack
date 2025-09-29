1163d05d53018ae230de6563e677006a90500a1b
sanya-padalko <apadalko90@gmail.com>
2025-09-29 16:13:18 +0300

    errors.cpp
    Функция парса ошибок

---
+++ src/errors.cpp
@@ -0,0 +1,34 @@
+#include "errors.h"
+
+void ParseErr(StackErr_t error_type) {
+    switch (error_type) {
+        case NOTHING:
+            printerr(GREEN_COLOR "Everything is OK\n");
+            break;
+        case NULLPTR:
+            printerr(RED_COLOR "Pointer went equal NULL\n");
+            break;
+        case SIZE_ERR:
+            printerr(RED_COLOR "Stack has unavailable size\n");
+            break;
+        case CAPACITY_ERR:
+            printerr(RED_COLOR "Stack has unavailable capactiy\n");
+            break;
+        case EMPTY_STACK:
+            printerr(RED_COLOR "Trying to work with empty stack\n");
+            break;
+        case CANARY_ERR:
+            printerr(RED_COLOR "The canaries have been changed\n");
+            break;
+        case REALLOC_ERR:
+            printerr(RED_COLOR "Realloc went wrong\n");
+            break;
+        case HASH_ERR:
+            printerr(RED_COLOR "Hash of stack went wrong. The inside of the stack has been changed\n");
+            break;
+        default:
+            printerr(YELLOW_COLOR "Unknown error\n");
+    }
+
+    printerr(RESET_COLOR);
+}

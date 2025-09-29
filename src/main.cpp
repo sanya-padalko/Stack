0cf6f1d418235e408ca0e5069f65cc06646e11c9
sanya-padalko <apadalko90@gmail.com>
2025-09-29 16:13:36 +0300

    main.cpp
    Пример работы со стеком

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "errors.h"
+#include "stack.h"
+#include "vars.h"
+#include "verify.h"
+
+int main() {
+
+    srand(time(NULL));
+
+    StackErr_t* code_error = (StackErr_t*)calloc(1, sizeof(StackErr_t));
+    *code_error = NOTHING;
+    
+    make_stack(stack, (ssize_t)2);
+
+    for (int i = 0; i < 400; ++i) {
+        if (i % 5 == 3) {
+            StackPop(stack, code_error);
+            continue;
+        }
+        StackPush(stack, rand() % (unsigned int)rand(), code_error);
+    }
+
+    for (int i = 0; i < 40; ++i) {
+        StackPop(stack, code_error);
+    }
+
+    stackverify(stack, 0);
+
+    stackdump(stack);
+
+    delete_stack(stack);
+}

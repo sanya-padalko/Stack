514d0afd6ceecb75da9c7eda1ed8816c7ccfb7fc
sanya-padalko <apadalko90@gmail.com>
2025-09-30 17:12:08 +0300

    Универсальный make_stack
    make_stack также теперь не требует code_error, поэтому его можно использовать любому

--- inc/stack.h
+++ inc/stack.h
@@ -7,24 +7,20 @@
 
 #ifdef DEBUG
 #define make_stack(name, capacity) \
-        if (code_error == NULL) \
-            code_error = (StackErr_t*)calloc(1, sizeof(StackErr_t)); \
-        stack_t* name = StackCtor(capacity + 2, code_error, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
+        stack_t* name = StackCtor(capacity + 2, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
 #define ON_DEBUG(...) __VA_ARGS__
 #else
 #define make_stack(name, capacity) \
-        if (code_error == NULL) \
-            code_error = (StackErr_t*)calloc(1, sizeof(StackErr_t)); \
-        stack_t* name = StackCtor(capacity + 2, code_error);
+        stack_t* name = StackCtor(capacity + 2);
 #define ON_DEBUG(...)
 #endif
 
-stack_t* StackCtor(ssize_t capacity, StackErr_t *code_erro ON_DEBUG(, VarInfo varinfo));
+stack_t* StackCtor(ssize_t capacity ON_DEBUG(, VarInfo varinfo));
 
-StackErr_t StackDtor(stack_t *stack, StackErr_t *code_error);
+StackErr_t StackDtor(stack_t *stack);
 
-void StackPush(stack_t *stack, StackElem_t x, StackErr_t *code_error);
+void StackPush(stack_t *stack, StackElem_t new_value);
 
-StackElem_t StackPop(stack_t *stack, StackErr_t *code_error);
+StackElem_t StackPop(stack_t *stack);
 
 #endif

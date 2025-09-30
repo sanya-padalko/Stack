77341ee8409a906460d1a5efaa963c3237654e2c
sanya-padalko <apadalko90@gmail.com>
2025-09-30 12:01:58 +0300

    StackCtor переделан
    Возвращает указатель на новый созданный стек

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,16 +1,20 @@
 #include "stack.h"
 
-StackErr_t StackCtor(stack_t *stack, ssize_t capacity ON_DEBUG(, VarInfo varinfo)) {
+stack_t* StackCtor(ssize_t capacity, StackErr_t *code_error ON_DEBUG(, VarInfo varinfo)) {
     ON_DEBUG(PrintVarInfo(varinfo));
 
+    stack_t *stack = (stack_t*)calloc(1, sizeof(stack_t));
+
     if (stack == NULL) {
-        printerr(RED_COLOR "Pointer went equal NULL in Stack's Constuctor\n" RESET_COLOR);
-        return NULLPTR;
+        *code_error = NULLPTR;
+        printerr(RED_COLOR "Calloc return NULL (during creating struct) in Stack's Constuctor\n" RESET_COLOR);
+        return NULL;
     }
 
     if (capacity > 10000000 || capacity < 2) {
+        *code_error = SIZE_ERR;
         printerr(RED_COLOR "Unavailable size of stack in Stack's Constructor\n" RESET_COLOR);
-        return SIZE_ERR;
+        return NULL;
     }
 
     stack->hash = 0;
@@ -21,22 +25,23 @@ StackErr_t StackCtor(stack_t *stack, ssize_t capacity ON_DEBUG(, VarInfo varinfo
 
     void* calloc_ptr = calloc(stack->capacity, sizeof(StackElem_t));
     if (calloc_ptr == NULL) {
+        *code_error = NULLPTR;
         printerr(RED_COLOR "Calloc return NULL in Stack Constructor\n" RESET_COLOR);
-        return NULLPTR;
+        return NULL;
     }
 
     stack->data = (StackElem_t*)calloc_ptr;
 
-    stack->data[0] = CANARY;
+    stack->data[0] = CANARY_LEFT;
 
-    stack->data[stack->capacity - 1] = CANADA;
+    stack->data[stack->capacity - 1] = CANARY_RIGHT;
 
     for (size_t i = 1; i < stack->capacity - 1; ++i) {
-        stack->data[i] = POIZON;
-        stack->hash = (stack->hash + i * POIZON % mod) % mod;
+        stack->data[i] = POIZON_VALUE;
+        stack->hash = (stack->hash + i * POIZON_VALUE % mod) % mod;
     }
 
-    return NOTHING;
+    return stack;
 }
 
 StackErr_t StackDtor(stack_t *stack, StackErr_t *code_error) {
@@ -50,10 +55,15 @@ StackErr_t StackDtor(stack_t *stack, StackErr_t *code_error) {
     return NOTHING;
 }
 
-static int expand(stack_t *stack, StackErr_t *code_error) {
+static int StackRealloc(stack_t *stack, ssize_t new_size, StackErr_t *code_error) {
     stackverify(stack, -1);
 
-    void* realloc_ptr = realloc(stack->data, stack->capacity * 2 * sizeof(StackElem_t));
+    if (new_size < stack->size) {
+        printerr(RED_COLOR "Unavailable realloc-size of stack\n" RESET_COLOR);
+        return -1;
+    }
+
+    void* realloc_ptr = realloc(stack->data, new_size * sizeof(StackElem_t));
     if (realloc_ptr == NULL) {
         *code_error = REALLOC_ERR;
         ON_DEBUG(stackdump(stack));
@@ -61,35 +71,25 @@ static int expand(stack_t *stack, StackErr_t *code_error) {
         return -1;
     }
 
-    stack->capacity *= 2;
-
     stack->data = (StackElem_t*)realloc_ptr;
 
-    stack->data[stack->capacity - 1] = CANADA;
+    stack->data[stack->capacity - 1] = POIZON_VALUE;
+    stack->hash = (stack->hash + (stack->capacity - 1) * POIZON_VALUE % mod) % mod;
 
-    for (int i = stack->capacity / 2 - 1; i < stack->capacity - 1; ++i) {
-        stack->data[i] = POIZON;
-        stack->hash = (stack->hash + i * POIZON % mod) % mod;
+    while (stack->capacity < new_size) {
+        ++stack->capacity;
+        stack->hash = (stack->hash + (stack->capacity - 1) * POIZON_VALUE) % mod;
+        stack->data[stack->capacity - 1] = POIZON_VALUE;
     }
 
-    return stack->capacity;
-}
-
-static int narrow(stack_t *stack, StackErr_t *code_error) {
-    stackverify(stack, -1);
-
-    stack->data[stack->capacity - 1] = POIZON;
-
-    for (int i = stack->size - 1; i < stack->capacity - 1; ++i) {
-        stack->hash = (stack->hash + mod - i * POIZON % mod) % mod;
+    while (stack->capacity > new_size) {
+        --stack->capacity;
+        stack->hash = (stack->hash + mod - stack->capacity * POIZON_VALUE % mod) % mod;
     }
 
-    stack->capacity = stack->size;
+    stack->data[stack->capacity - 1] = CANARY_RIGHT;
+    stack->hash = (stack->hash + mod - (stack->capacity - 1) * POIZON_VALUE % mod) % mod;
 
-    stack->data = (StackElem_t*)realloc(stack->data, stack->capacity * sizeof(StackElem_t));
-
-    stack->data[stack->capacity - 1] = CANADA;
-    
     return stack->capacity;
 }
 
@@ -97,7 +97,7 @@ void StackPush(stack_t *stack, StackElem_t x, StackErr_t *code_error) {
     stackverify(stack, );
 
     if (stack->size == stack->capacity) {
-        expand(stack, code_error);
+        StackRealloc(stack, stack->size * 2, code_error);
 
         if (*code_error != NOTHING) {
             printerr(RED_COLOR "Push was not completed\n" RESET_COLOR);
@@ -106,7 +106,7 @@ void StackPush(stack_t *stack, StackElem_t x, StackErr_t *code_error) {
     }
 
     *(stack->data + stack->size - 1) = x;
-    stack->hash = (stack->hash + mod - (stack->size - 1) * POIZON % mod) % mod;
+    stack->hash = (stack->hash + mod - (stack->size - 1) * POIZON_VALUE % mod) % mod;
     stack->hash = (stack->hash + (stack->size - 1) * x) % mod;
 
     ++stack->size;
@@ -115,27 +115,27 @@ void StackPush(stack_t *stack, StackElem_t x, StackErr_t *code_error) {
 }
 
 StackElem_t StackPop(stack_t *stack, StackErr_t *code_error) {
-    stackverify(stack, POIZON);
+    stackverify(stack, POIZON_VALUE);
 
     if (stack->size == 2) {
         *code_error = EMPTY_STACK;
         ON_DEBUG(stackdump(stack));
         ParseErr(*code_error);
         printerr(RED_COLOR "You cannot pop from empty stack\n\n" RESET_COLOR);
-        return POIZON;
+        return POIZON_VALUE;
     }
 
     --stack->size;
     StackElem_t last = *(stack->data + stack->size - 1);
-    *(stack->data + stack->size - 1) = POIZON;
+    *(stack->data + stack->size - 1) = POIZON_VALUE;
     stack->hash = (stack->hash + mod - (stack->size - 1) * last % mod) % mod;
-    stack->hash = (stack->hash + (stack->size - 1) * POIZON % mod) % mod;
+    stack->hash = (stack->hash + (stack->size - 1) * POIZON_VALUE % mod) % mod;
 
     if (stack->size * 4 == stack->capacity)
-        narrow(stack, code_error);
+        StackRealloc(stack, stack->size, code_error);
 
     if (*code_error != NOTHING)
-        return POIZON;
+        return POIZON_VALUE;
 
     return last;
 }

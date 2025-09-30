c5f1fadbcc026dad72e9196057829a021c2226bf
sanya-padalko <apadalko90@gmail.com>
2025-09-30 12:01:12 +0300

    Переделан #define make_stack
    Calloc для переменной делается в конструкторе

--- inc/stack.h
+++ inc/stack.h
@@ -3,32 +3,23 @@
 
 #include <stdlib.h>
 #include "errors.h"
-#include "vars.h"
 #include "verify.h"
 
 #ifdef DEBUG
 #define make_stack(name, capacity) \
-        stack_t* name = (stack_t*)calloc(1, sizeof(stack_t)); \
         if (code_error == NULL) \
             code_error = (StackErr_t*)calloc(1, sizeof(StackErr_t)); \
-        *code_error = StackCtor(name, capacity + 2, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
+        stack_t* name = StackCtor(capacity + 2, code_error, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
 #define ON_DEBUG(...) __VA_ARGS__
 #else
 #define make_stack(name, capacity) \
-        stack_t* name = (stack_t*)calloc(1, sizeof(stack_t)); \
         if (code_error == NULL) \
             code_error = (StackErr_t*)calloc(1, sizeof(StackErr_t)); \
-        *code_error = StackCtor(name, capacity + 2);
+        stack_t* name = StackCtor(capacity + 2, code_error);
 #define ON_DEBUG(...)
 #endif
 
-#define delete_stack(stack) StackDtor(stack, code_error);
-
-#define get_name(x) #x
-
-#define base(type) *((type*)calloc(1, sizeof(type)))
-
-StackErr_t StackCtor(stack_t *stack, ssize_t capacity ON_DEBUG(, VarInfo varinfo));
+stack_t* StackCtor(ssize_t capacity, StackErr_t *code_erro ON_DEBUG(, VarInfo varinfo));
 
 StackErr_t StackDtor(stack_t *stack, StackErr_t *code_error);
 
